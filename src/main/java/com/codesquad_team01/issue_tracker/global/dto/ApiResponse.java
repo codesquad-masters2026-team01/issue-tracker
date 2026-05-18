e7605a48f23926fbfcb482fc6fcb8d3f6dc3424f
LeeJaeWan <jjjkuul@naver.com>
2026-05-18 17:46:09 +0900

    Feat: 공통 데이터를 담기 위한 global Record ApiResponse
    - [ApiResponse]: 모든 컨트롤러에서 공통적으로 반환해야 하는 표준 규격
    - boolean success, String message, T data

---
+++ src/main/java/com/codesquad_team01/issue_tracker/global/dto/ApiResponse.java
@@ -0,0 +1,12 @@
+package com.codesquad_team01.issue_tracker.global.dto;
+
+public record ApiResponse<T> (
+        boolean success,
+        String message,
+        T data
+){
+
+    public static <T> ApiResponse<T> ok(String message, T data){
+        return new ApiResponse<>(true, message, data);
+    }
+}
