58c8a04de907eaef30ef3c1e629fde21eae188bb
LeeJaeWan <jjjkuul@naver.com>
2026-05-20 07:59:21 +0900

    Feat: 레이블 추가 Controller 메서드 작성
    - 리소스 생성 성공을 명시하기 위해 기본 200 OK 대신 201 Created 상태 코드를 반환하도록 @ResponseStatus 설정
    - 입력 데이터의 신뢰성을 웹 계층 경계에서 검증(@Valid)하고, 불변 객체인 LabelAddRequest 레코드로 역직렬화
    - 비즈니스 계층으로 처리를 위임한 후, 생성 완료된 결과를 LabelAddResponse 레코드에 담아 클라이언트에게 최종 응답

--- src/main/java/com/codesquad_team01/issue_tracker/label/controller/LabelController.java
+++ src/main/java/com/codesquad_team01/issue_tracker/label/controller/LabelController.java
@@ -1,11 +1,13 @@
 package com.codesquad_team01.issue_tracker.label.controller;
 
 import com.codesquad_team01.issue_tracker.global.dto.ApiResponse;
+import com.codesquad_team01.issue_tracker.label.dto.request.LabelAddRequest;
+import com.codesquad_team01.issue_tracker.label.dto.response.LabelAddResponse;
 import com.codesquad_team01.issue_tracker.label.dto.response.LabelPageResponse;
 import com.codesquad_team01.issue_tracker.label.service.LabelService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/labels")
@@ -16,9 +18,17 @@ public class LabelController {
         this.labelService = labelService;
     }
 
+    // TODO: 레이블 정렬(가장 최근 생긴 레이블이 위로 간다), 삭제된 인스턴스 고려
     @GetMapping
     public ApiResponse<LabelPageResponse> getLabels() {
         LabelPageResponse responseData = labelService.getLabelPageResponse();
         return ApiResponse.success("레이블 페이지 로딩 성공", responseData);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResponse<LabelAddResponse> addLabel(@Valid @RequestBody LabelAddRequest labelAddRequest) {
+        LabelAddResponse responseData = labelService.addLabel(labelAddRequest);
+        return ApiResponse.success("레이블 추가 성공", responseData);
     }
 }
