3e44a5a1acb77982a0752b387f9c1edcfd6809d4
LeeJaeWan <jjjkuul@naver.com>
2026-05-19 15:16:32 +0900

    Feat: LabelController, LabelService 병합
    - [LabelController]: List<Label>이 아닌 List<LabelListResponse>를 반환하도록 수정
    - [LabelService]: 이전과 동일

--- src/main/java/com/codesquad_team01/issue_tracker/label/controller/LabelController.java
+++ src/main/java/com/codesquad_team01/issue_tracker/label/controller/LabelController.java
@@ -1,12 +1,11 @@
 package com.codesquad_team01.issue_tracker.label.controller;
 
 import com.codesquad_team01.issue_tracker.global.dto.ApiResponse;
-import com.codesquad_team01.issue_tracker.label.domain.Label;
+import com.codesquad_team01.issue_tracker.label.dto.response.LabelPageResponse;
 import com.codesquad_team01.issue_tracker.label.service.LabelService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/labels")
@@ -18,7 +17,8 @@ public class LabelController {
     }
 
     @GetMapping
-    public ApiResponse<List<Label>> getLabels() {
-        return ApiResponse.success(labelService.findAll());
+    public ApiResponse<LabelPageResponse> getLabels() {
+        LabelPageResponse responseData = labelService.getLabelPageResponse();
+        return ApiResponse.success("레이블 페이지 로딩 성공", responseData);
     }
 }
--- src/main/java/com/codesquad_team01/issue_tracker/label/dto/LabelPageResponse.java
+++
@@ -1,8 +0,0 @@
-package com.codesquad_team01.issue_tracker.label.dto;
-
-import java.util.List;
-
-public record LabelPageResponse (
-        LabelMetaData metadata,
-        List<LabelTempResponse> labels
-){}
