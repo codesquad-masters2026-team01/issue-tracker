53d805f9741ed497f223e872fbe8893a6eaf1d9e
LeeJaeWan <jjjkuul@naver.com>
2026-05-19 14:19:44 +0900

    dev Pull 전 수정 사항 점검

--- src/test/java/com/codesquad_team01/issue_tracker/label/LabelControllerTest.java
+++ src/test/java/com/codesquad_team01/issue_tracker/label/LabelControllerTest.java
@@ -27,12 +27,6 @@ public class LabelControllerTest {
     @MockitoBean
     private LabelService labelService;
 
-    /* TODO
-        1. 요청(Request) 검증: 올바른 URL, HTTP Method(GET), Header(Authorization)를 잘 매핑하여 받아들이는가?
-        2. 응답(Response) 검증: 우리가 설계한 DTO 구조가 올바른 JSON 포맷으로 직렬화(Serialization)되어 반환되는가?
-        3. 상태 코드(Status Code): 상황에 맞는 HTTP 상태 코드(200, 401 등)를 반환하는가?
-     */
-
     @Test
     @DisplayName("GET /api/labels 요청이 온 뒤 정상적으로 라벨 목록과 마일스톤의 개수를 조회하면 200 OK와 통합 데이터를 반환한다.")
     public void getLabels_Success() throws Exception {
