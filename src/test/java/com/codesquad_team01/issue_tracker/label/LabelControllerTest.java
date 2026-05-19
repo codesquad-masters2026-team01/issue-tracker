8006effb6979d15918d513b9b5f31b4532ebe9b4
LeeJaeWan <jjjkuul@naver.com>
2026-05-19 18:09:00 +0900

    Test: LabelController의 addLabel() 웹 계층 통합 테스트 구현
    - 비즈니스 계층(LabelService)을 Mocking하여 컨트롤러의 책임만 독립적으로 격리 및 검증
    - [상태 검증] POST /api/labels 요청 시 HTTP 상태 코드(201 Created) 및 ApiResponse의 표준 응답 포맷(success, message, data) 정상 반환 확인
    - [행위 검증] ArgumentCaptor를 활용하여, 클라이언트의 JSON 요청이 LabelAddRequest DTO로 정확히 역직렬화되어 서비스 계층에 누락 없이 위임되는지 확인

--- src/test/java/com/codesquad_team01/issue_tracker/label/LabelControllerTest.java
+++ src/test/java/com/codesquad_team01/issue_tracker/label/LabelControllerTest.java
@@ -1,22 +1,32 @@
 package com.codesquad_team01.issue_tracker.label;
 
 import com.codesquad_team01.issue_tracker.label.controller.LabelController;
+import com.codesquad_team01.issue_tracker.label.dto.request.LabelAddRequest;
+import com.codesquad_team01.issue_tracker.label.dto.response.LabelAddResponse;
 import com.codesquad_team01.issue_tracker.label.dto.response.LabelListResponse;
 import com.codesquad_team01.issue_tracker.label.dto.response.LabelMetaData;
 import com.codesquad_team01.issue_tracker.label.dto.response.LabelPageResponse;
 import com.codesquad_team01.issue_tracker.label.service.LabelService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -28,9 +38,11 @@ public class LabelControllerTest {
 
     @MockitoBean
     private LabelService labelService;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
-    @DisplayName("GET /api/labels 요청이 온 뒤 정상적으로 라벨 목록과 마일스톤의 개수를 조회하면 200 OK와 통합 데이터를 반환한다.")
+    @DisplayName("GET /api/labels 요청이 온 뒤 정상적으로 레이블 목록과 마일스톤의 개수를 조회하면 200 OK와 통합 데이터를 반환한다.")
     public void getLabels_Success() throws Exception {
         LabelMetaData labelMetaData = new LabelMetaData(3, 1);
         LabelListResponse label1 = new LabelListResponse(1L, "라벨1", "라벨1의 설명", "#000000", "#FCFBFB");
@@ -56,5 +68,48 @@ public class LabelControllerTest {
                 .andExpect(jsonPath("$.data.labels[0].description").exists())
                 .andExpect(jsonPath("$.data.labels[0].textColor").exists())
                 .andExpect(jsonPath("$.data.labels[0].backgroundColor").exists());
+    }
+
+    @Test
+    @DisplayName("POST /api/labels 요청이 온 뒤 정상적으로 DB에 레이블을 추가하면 " +
+            "추가한 레이블의 정보를 담은 LabelCreatedResponse를 반환받고, 201 Created와 해당 데이터를 반환한다.")
+    public void addLabel_Success() throws Exception {
+        LabelAddRequest requestDto = new LabelAddRequest(
+                "documentation",
+                "서비스에 대한 개선 사항 또는 추가 사항",
+                "#000000",
+                "#004DE3"
+        );
+
+        LabelAddResponse mockResponseDto = new LabelAddResponse(
+                4L,
+                "documentation",
+                "서비스에 대한 개선 사항 또는 추가 사항",
+                "#000000",
+                "#004DE3");
+
+        given(labelService.addLabel(any(LabelAddRequest.class))).willReturn(mockResponseDto);
+
+        mockMvc.perform(
+                post("/api/labels")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestDto))
+        )
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.message").value("레이블 추가 성공"))
+                .andExpect(jsonPath("$.data.id").value(4L))
+                .andExpect(jsonPath("$.data.name").value("documentation"))
+                .andExpect(jsonPath("$.data.description").value("서비스에 대한 개선 사항 또는 추가 사항"))
+                .andExpect(jsonPath("$.data.textColor").value("#000000"))
+                .andExpect(jsonPath("$.data.backgroundColor").value("#004DE3"));
+
+        ArgumentCaptor<LabelAddRequest> requestCaptor = ArgumentCaptor.forClass(LabelAddRequest.class);
+        then(labelService).should(times(1)).addLabel(requestCaptor.capture());
+        LabelAddRequest capturedRequest = requestCaptor.getValue();
+        assertThat(capturedRequest.name()).isEqualTo("documentation");
+        assertThat(capturedRequest.description()).isEqualTo("서비스에 대한 개선 사항 또는 추가 사항");
+        assertThat(capturedRequest.textColor()).isEqualTo("#000000");
+        assertThat(capturedRequest.backgroundColor()).isEqualTo("#004DE3");
     }
 }
