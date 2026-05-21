79aa08463e4076a4713a31d3a2aac8e97b4238f8
LeeJaeWan <jjjkuul@naver.com>
2026-05-22 05:05:08 +0900

    Feat: MilestoneService 구현
    - MilestoneRepository의 findAllByState() 메서드의 반환값으로 List<MilestoneListItemResponse>를 받는다.
    - 이것을 MilestoneListResponse 객체로 감싸서 Controller에 반환

--- src/main/java/com/codesquad_team01/issue_tracker/milestone/service/MilestoneService.java
+++ src/main/java/com/codesquad_team01/issue_tracker/milestone/service/MilestoneService.java
@@ -1,9 +1,8 @@
 package com.codesquad_team01.issue_tracker.milestone.service;
-
-import com.codesquad_team01.issue_tracker.milestone.domain.Milestone;
+import com.codesquad_team01.issue_tracker.milestone.domain.MilestoneState;
+import com.codesquad_team01.issue_tracker.milestone.dto.response.MilestoneListResponse;
 import com.codesquad_team01.issue_tracker.milestone.repository.MilestoneRepository;
 import org.springframework.stereotype.Service;
-import java.util.List;
 
 @Service
 public class MilestoneService {
@@ -13,7 +12,7 @@ public class MilestoneService {
         this.milestoneRepository = milestoneRepository;
     }
 
-    public List<Milestone> findAll() {
-        return milestoneRepository.findAll();
+    public MilestoneListResponse findMilestones(MilestoneState state){
+        return new MilestoneListResponse(milestoneRepository.findAllByState(state));
     }
 }
