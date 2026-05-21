769666ac1cab68cbae8f7fa866ac860275eac8c2
LeeJaeWan <jjjkuul@naver.com>
2026-05-22 05:00:51 +0900

    Feat: MilestoneController getMilestones() 구현
    - 쿼리 스트링으로 들어오는 state="open"/"closed"를 Enum 객체로 받기 위해 StringToMilestoneStateConverter 구현 -> 원활한 상태 관리
    - 상태에 따라 열린 혹은 닫힌 마일스톤 메세지와 객체를 반환하도록 구현

--- src/main/java/com/codesquad_team01/issue_tracker/milestone/controller/MilestoneController.java
+++ src/main/java/com/codesquad_team01/issue_tracker/milestone/controller/MilestoneController.java
@@ -1,12 +1,13 @@
 package com.codesquad_team01.issue_tracker.milestone.controller;
 
 import com.codesquad_team01.issue_tracker.global.dto.ApiResponse;
-import com.codesquad_team01.issue_tracker.milestone.domain.Milestone;
+import com.codesquad_team01.issue_tracker.milestone.domain.MilestoneState;
+import com.codesquad_team01.issue_tracker.milestone.dto.response.MilestoneListResponse;
 import com.codesquad_team01.issue_tracker.milestone.service.MilestoneService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/milestones")
@@ -18,7 +19,11 @@ public class MilestoneController {
     }
 
     @GetMapping
-    public ApiResponse<List<Milestone>> getMilestones() {
-        return ApiResponse.success(milestoneService.findAll());
+    public ApiResponse<MilestoneListResponse> getMilestones(
+            @RequestParam("state") MilestoneState state
+    ) {
+        String message = state == MilestoneState.OPEN ?
+                "열린 모든 마일스톤 조회 성공" : "닫힌 모든 마일스톤 조회 성공";
+        return ApiResponse.success(message, milestoneService.findMilestones(state));
     }
 }
---
+++ src/main/java/com/codesquad_team01/issue_tracker/milestone/domain/MilestoneState.java
@@ -0,0 +1,6 @@
+package com.codesquad_team01.issue_tracker.milestone.domain;
+
+public enum MilestoneState {
+    OPEN(),
+    CLOSED()
+}
