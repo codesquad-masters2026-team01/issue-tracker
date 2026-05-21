965fb843a2714c7c6fae0eefb0b14c8b606cbd5e
LeeJaeWan <jjjkuul@naver.com>
2026-05-22 05:06:49 +0900

    Feat: MilestoneRepository findAllByState() 구현
    - 일반 마일스톤의 정보와 각 마일스톤과 연결된 열린/닫힌 이슈들의 개수를 데이터로 포함시켜야했음
    - 이를 위해 스칼라 쿼리를 활용 -> DB의 작업량이 마일스톤 개수가 늘어남에 따라 선형적으로 늘 수 있음
    - 하지만 마일스톤의 양은 비즈니스 흐름 상 많아지지 않는다고 판단
    - 추후 성능상 문제가 생기면 조건부 집계(CASE WHEN) + JOIN + GROUP BY 를 통해 DB내 작업량을 줄일 예정

--- src/main/java/com/codesquad_team01/issue_tracker/milestone/repository/MilestoneRepository.java
+++ src/main/java/com/codesquad_team01/issue_tracker/milestone/repository/MilestoneRepository.java
@@ -1,8 +1,11 @@
 package com.codesquad_team01.issue_tracker.milestone.repository;
 
 import com.codesquad_team01.issue_tracker.milestone.domain.Milestone;
+import com.codesquad_team01.issue_tracker.milestone.domain.MilestoneState;
+import com.codesquad_team01.issue_tracker.milestone.dto.response.MilestoneListItemResponse;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,4 +15,18 @@ public interface MilestoneRepository extends ListCrudRepository<Milestone, Long>
             "AND deleted_at is null")
     List<Milestone> findByName(String name);
     long countByDeletedAtIsNull();
+
+    @Query("SELECT " +
+            "m.id , " +
+            "m.name,  " +
+            "m.completion_date,  " +
+            "m.description, " +
+            "(SELECT COUNT(*) FROM issue i WHERE m.id = i.milestone_id AND i.is_opened = 1 AND i.deleted_at IS NULL) " +
+            "AS open_issue_num, " +
+            "(SELECT COUNT(*) FROM issue i WHERE i.milestone_id = m.id AND i.is_opened = 0 AND i.deleted_at IS NULL) " +
+            "AS closed_issue_num " +
+            "FROM milestone m " +
+            "WHERE m.is_opened = :#{#state.name() == 'OPEN' ? 1 : 0} AND m.deleted_at IS NULL " +
+            "ORDER BY m.id DESC")
+    List<MilestoneListItemResponse> findAllByState(@Param("state") MilestoneState state);
 }
